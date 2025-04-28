f78fd43227af4f1dcbeb500a03a4541c4d154ec7
Cristian <cristian.martinez.716cm9@gmail.com>
2025-04-27 20:09:48 -0600

    s

--- djangocrud/djangocrud/settings.py
+++ djangocrud/djangocrud/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': os.getenv('PGUSER'),
         'PASSWORD': os.getenv('PGPASSWORD'),
         'HOST': os.getenv('PGHOST'),
-        'PORT': os.getenv('PGPORT'),
+        'PORT': os.getenv('PGPORT', '5432'),  # 5432 es el puerto por defecto
     }
 }
 
