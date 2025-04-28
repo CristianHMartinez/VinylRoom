ad3befbb4c1b3775937cffd97352ccd45845de99
Cristian <cristian.martinez.716cm9@gmail.com>
2025-04-27 21:19:59 -0600

    host

--- djangocrud/djangocrud/settings.py
+++ djangocrud/djangocrud/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-=_d9z*$q7de#ta#v9$mg*sl9^2c9m^ft)_zanf%z=mi(ul$c5v
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = [os.getenv('RAILWAY_PUBLIC_DOMAIN', '*')]
+ALLOWED_HOSTS = ['.railway.app', 'vinylroom.up.railway.app', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
