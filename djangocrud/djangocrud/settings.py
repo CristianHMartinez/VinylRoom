fd09320bd1eaec7eefff193c3466879b04211ef0
Cristian <cristian.martinez.716cm9@gmail.com>
2025-04-27 22:41:41 -0600

    statics

--- djangocrud/djangocrud/settings.py
+++ djangocrud/djangocrud/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'catalog/static/images', 
+]
 
 # CLOUDFLARE R2 settings
 CLOUDFLARE_R2_BUCKET=config("CLOUDFLARE_R2_BUCKET")
